f2efac4a6829721b622f2b2871d305e577abd04b
svadhis <imeian@gmail.com>
2022-06-28 10:53:06 +0200

    ++

--- src/Count.js
+++ src/Count.js
@@ -3,15 +3,9 @@ import React from "react";
 export default class Count extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            count: 0
-        };
     }
 
     increment = () => {
-        this.setState({
-            count: this.state.count + 1
-        })
     }
 
     render() {
